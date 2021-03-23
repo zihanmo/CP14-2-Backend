09b8b5562be51c55d2a5c3acf5336c6b7216f482
zihan Mo <mozihan620@gmail.com>
2021-03-23 19:50:07 +0800

    changedbname

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,6 +2,6 @@ const mongoose=require('mongoose');
 
 
 exports.connectToDB=()=>{
-    const connectionString=`mongodb+srv://CP142DB:KwJkcNE9h3kkx4pE@cluster0.snhy6.mongodb.net/CP12DB?retryWrites=true&w=majority`
+    const connectionString=`mongodb+srv://CP142DB:KwJkcNE9h3kkx4pE@cluster0.snhy6.mongodb.net/CP142DB?retryWrites=true&w=majority`
     return mongoose.connect(connectionString);
 }
