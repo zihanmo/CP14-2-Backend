089c917844a496159d628b724e38bcc55a043037
zihan Mo <mozihan620@gmail.com>
2021-03-23 19:44:27 +0800

    connect to cloud

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,8 +2,6 @@ const mongoose=require('mongoose');
 
 
 exports.connectToDB=()=>{
-    const {DB_DATABASE, DB_HOST,DB_PORT}=process.env;
-    const connectionString=`mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`
+    const connectionString=`mongodb+srv://CP142DB:KwJkcNE9h3kkx4pE@cluster0.snhy6.mongodb.net/CP12DB?retryWrites=true&w=majority`
     return mongoose.connect(connectionString);
-
 }
