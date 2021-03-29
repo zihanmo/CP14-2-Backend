64ecc4945de1ffe76c7c0c6e3a54e0aa874ca58f
zihan Mo <mozihan620@gmail.com>
2021-03-29 23:20:10 +0800

    same email can not register

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -5,6 +5,10 @@ const { generateToken } = require("../utils/jwt");
 
 async function addUser(req, res) {
   const { email, password, role, fullName, gender, dob, staffId } = req.body;
+  const existingUser = await User.findOne({ email }).exec();
+  if (existingUser) {
+    return res.status(401).json("email is already taken");
+  }
   const user = new User({
     email,
     password,
