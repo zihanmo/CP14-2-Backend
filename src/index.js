1f4818666080a1cbf895bc7e7d35603b50d43f24
Xiaowei Zhang <zxw971118@gmail.com>
2021-05-08 17:01:13 +1000

    fix file upload can not upload larger file bug

--- src/index.js
+++ src/index.js
@@ -11,37 +11,31 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 
 app.use(cors({ origin: "*" }));
-app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "15mB" }));
 
 app.use("/api", routes);
 app.use(errorHandler);
-var fileupload = require("express-fileupload");
-app.use(
-  fileupload({
-    useTempFiles: true
-  })
-);
 
 var cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "dzjg12m3b",
   api_key: "518216572745741",
-  api_secret: "8qKRPoZpIIYmdB4CG2kVUgTwqj0"
+  api_secret: "8qKRPoZpIIYmdB4CG2kVUgTwqj0",
 });
 
 connectToDB()
   .then(() => {})
-  .catch(e => {
+  .catch((e) => {
     console.error(e);
     process.exit(1);
   });
 
 app.get("/", (req, res) => res.send("Welcome to backend"));
-app.post("/upload", function(req, res, next) {
-  var data = JSON.parse(req.body.file);
-  var image = data.uri;
-  cloudinary.uploader.upload(image, function(err, result) {
+app.post("/upload", function (req, res, next) {
+  cloudinary.uploader.upload(req.body.uri, function (err, result) {
     res.send(result);
   });
 });
